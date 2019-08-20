9719983f110cd5ce386d1d88218e0b4b77f54d8c
Takeshi Kimoto <lnuk2009jp@gmail.com>
2019-08-20 17:20:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,10 @@
 # require modules here
+require 'pry'
 require "yaml"
 
 def load_library(file_path)
   emoticons = YAML.load_file(file_path)
-
+binding.pry
   new_hash = {
     'get_meaning' => {japanese_emoticon => 1},
     'get_emoticon' => {}
